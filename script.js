f1976deb2d5744e754dddac67fda688b6f321afa
David <popadavid843@yahoo.com>
2022-03-01 17:36:03 +0200

    85% ccompleted

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const header = document.querySelector(".all");
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 50);
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  const update = () => {
+    const target = counter.getAttribute("data-target"); // string
+    const transStrToNrDataTarget = Number(target);
+    const transStrToNrCounterInnerText = Number(counter.innerText); // nr
+    if (transStrToNrCounterInnerText < transStrToNrDataTarget) {
+      counter.innerHTML = Math.trunc(transStrToNrCounterInnerText + 2);
+      setTimeout(update, 10); //cheama functia update peste jumatate de secunda
+    }
+    // pleaca de la 0 - data-target pt fiecare
+  };
+
+  update();
 });
