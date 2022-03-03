e87affe54fb40b926a23023a79517f1bdcff3f58
David <popadavid843@yahoo.com>
2022-03-03 14:18:00 +0200

    100%done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector(".all");
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 400);
 });
 
 const counters = document.querySelectorAll(".counter");
@@ -12,10 +12,35 @@ counters.forEach((counter) => {
     const transStrToNrCounterInnerText = Number(counter.innerText); // nr
     if (transStrToNrCounterInnerText < transStrToNrDataTarget) {
       counter.innerHTML = Math.trunc(transStrToNrCounterInnerText + 2);
-      setTimeout(update, 10); //cheama functia update peste jumatate de secunda
+      setTimeout(update, 50); //cheama functia update peste jumatate de secunda
     }
     // pleaca de la 0 - data-target pt fiecare
   };
 
   update();
+});
+
+const accordionItems = document.querySelectorAll(".accordion-item-header");
+accordionItems.forEach((accordionItem) => {
+  accordionItem.addEventListener("click", (e) => {
+    accordionItem.classList.toggle("active");
+    const accordionItemBody = accordionItem.nextElementSibling;
+    if (accordionItem.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+      console.log(accordionItemBody.style.maxHeight);
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+
+    const currentActiveAccordionItem = document.querySelector(
+      ".accordion-item-header.active"
+    );
+    if (
+      currentActiveAccordionItem &&
+      currentActiveAccordionItem !== accordionItem
+    ) {
+      currentActiveAccordionItem.classList.toggle("active");
+      currentActiveAccordionItem.nextElementSibling.style.maxHeight = 0;
+    }
+  });
 });
